07a22477ceda86d57c9d5bac83fb185e1bde7688
Zeynep Beyza <zynpbyza0@gmail.com>
2022-09-23 14:41:05 +0300

    search box design

--- src/component/StarshipList.js
+++ src/component/StarshipList.js
@@ -32,13 +32,23 @@ export default function StarshipList() {
 
   return (
     <>
-      <div style={{ padding: 10 }}>
-        <input
-          icon="search"
-          placeholder="Search..."
-          onChange={(event) => setSearchInput(event.target.value)}
+    
+<form class="flex items-center">   
+    <label for="simple-search" class="sr-only">Search</label>
+    <div class="relative w-full ml-10">
+        <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none ">
+            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+        </div>
+        <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required
+           onChange={(event) => setSearchInput(event.target.value)}
         />
-      </div>
+    </div>
+    <button type="submit" class="p-2.5 ml-4 mr-12 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+        <span class="sr-only">Search</span>
+    </button>
+</form>
+    
 
       <div className="grid grid-cols- gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         {post
@@ -88,6 +98,9 @@ export default function StarshipList() {
                       </h3>
                       <p className="mt-1 text-lg font-medium text-white">
                         {starships.model}
+                      </p>
+                      <p className="mt-1 text-lg font-medium text-white">
+                        {starships.hyperdrive_rating}
                       </p>
                     </a>
                   </div>
