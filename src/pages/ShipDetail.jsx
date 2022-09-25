1e875ea91b16176e6f3998891ca5608f36f6b737
Zeynep Beyza <zynpbyza0@gmail.com>
2022-09-25 18:29:17 +0300

    Font family eklendi

--- src/pages/ShipDetail.jsx
+++ src/pages/ShipDetail.jsx
@@ -2,7 +2,6 @@ import { useParams } from "react-router";
 import * as React from "react";
 import axios from "axios";
 import MyImage from "../component/MyImage";
-import BackButton from "../component/BackButton";
 
 const client = axios.create({
   baseURL: "https://swapi.dev/api/starships/",
@@ -35,11 +34,11 @@ export default function ShipDetail() {
       <div className="flex justify-center">
         <div class="max-w-sm bg-white rounded-lg border border-gray-500 shadow-md dark:bg-gray-800 dark:border-gray-700">
           <div class="p-5">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight font-mono text-gray dark:text-white , ">
               {post.name}
             </h5>
             <MyImage url={imgUrl + id + ".jpg"}></MyImage>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            <p class="mb-3 font-normal font-mono text-gray-700 dark:text-gray-400 ">
               Model: {post.model}
             </p>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
